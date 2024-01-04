0f8b162d421420689ded1164db3ba3bdd0cb67bf
Mahdi Alizadeh <mahdi.alizadeh.sp@gmail.com>
2024-01-04 23:14:56 +0330

    finish a2

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 ### Activity 1 - Functions
 
 Displays your first and last name
+
+### Activity 2 - Basic arithmetic
+
+Displays the result of the sum of two numbers
--- src/bin/a2.rs
+++ src/bin/a2.rs
@@ -8,4 +8,23 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn sum(a : i32, b: i32) -> i32 {
+    a + b
+}
+
+fn display_result(a : i32, b: i32, result : i32) {
+
+    println!("The result of sum {:?} and {:?} is {:?}", a, b, result);
+
+}
+
+fn main() {
+
+    let number1 : i32 = 3;
+    let number2 : i32 = 5;
+
+    let my_number: i32 = sum(number1, number2);
+
+    display_result(number1, number2, my_number)
+
+}
