55c214b8edc303f98e4a32319b04b2c4c0da3c80
Mahdi Alizadeh <mahdi.alizadeh.sp@gmail.com>
2024-01-04 19:19:35 +0330

    finish a1

---
+++ README.md
@@ -0,0 +1,7 @@
+# Rust Activities (zero to mastery)
+
+## [Rust Programming: The Complete Developer's Guide](https://zerotomastery.io/courses/learn-rust/)
+---
+### Activity 1 - Functions
+
+Displays your first and last name
--- src/bin/a1.rs
+++ src/bin/a1.rs
@@ -8,4 +8,23 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn display_name(name : &str) -> String {
+
+    format!("My name is {}", name)
+
+}
+
+fn display_last_name(name : &str) -> String {
+
+    format!("my last name is {}", name)
+
+}
+
+fn main() {
+
+    let name: String = display_name("Mahdi");
+    let last_name: String = display_last_name("Alizadeh");
+
+    println!("{} and {}", name, last_name);
+
+}
